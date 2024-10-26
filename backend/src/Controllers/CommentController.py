651d3b0334399640ce001a45e8e29d0ffa8e1a1e
knyaz7 <vn.guitar@mail.ru>
2024-10-26 21:51:33 +0300

    Various changes

--- backend/src/Controllers/CommentController.py
+++ backend/src/Controllers/CommentController.py
@@ -10,6 +10,13 @@ from src.Models.Task import Task
 
 
 class CommentController:
+    @staticmethod
+    async def get_one(comment_id: int, session: AsyncSession) -> CommentOutput:
+        result = await session.execute(select(Task).where(Comment.id == comment_id))
+        comment = result.scalar_one_or_none()
+        if comment is None:
+            raise HTTPException(status_code=404, detail="Задача не найдена")
+        return CommentOutput.from_orm(comment)
 
     @staticmethod
     async def create(comment: CommentInput, session: AsyncSession) -> CommentOutput:
@@ -45,14 +52,3 @@ class CommentController:
 
         await session.delete(comment)
         await session.commit()
-
-    @staticmethod
-    async def get_by_task(task_id: int, session: AsyncSession) -> List[CommentOutput]:
-        task = await session.execute(select(Task).where(Task.id == task_id))
-        task = task.scalar_one_or_none()
-        comment_ids = task.comments
-        query = await session.execute(select(Comment).where(Comment.id.in_(comment_ids)))
-        comments = query.scalars().all()
-        await session.commit()
-
-        return [CommentOutput.from_orm(comment) for comment in comments]
--- backend/src/Schemas/TaskSchemas.py
+++ backend/src/Schemas/TaskSchemas.py
@@ -3,6 +3,8 @@ from typing import Optional, List
 from pydantic import BaseModel
 
 from src.Schemas.CommentSchemas import CommentOutput
+from src.Schemas.PointSchemas import PointOutput
+from src.Schemas.ToolSchemas import ToolOutput
 
 
 class TaskInput(BaseModel):
@@ -30,9 +32,9 @@ class TaskOutput(BaseModel):
     user_id: int
     title: str
     description: str
-    stack: List[int]
+    stack: List[ToolOutput]
     position: int
-    points: List[int]
+    points: List[PointOutput]
     comments: List[CommentOutput]
 
     class Config:
