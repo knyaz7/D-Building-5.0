7357abda93587d219e738eb7913f2ac7a7e9fc76
knyaz7 <vn.guitar@mail.ru>
2024-10-26 22:45:59 +0300

    Various changes

--- backend/src/Routers/v1/task_router.py
+++ backend/src/Routers/v1/task_router.py
@@ -7,7 +7,10 @@ from starlette import status
 from src.Config.db import get_session
 from src.Controllers.AuthController import AuthController, oauth2_scheme
 from src.Controllers.TaskController import TaskController
+from src.Schemas.CommentSchemas import CommentInput, CommentOutput
+from src.Schemas.PointSchemas import PointInput, PointOutput
 from src.Schemas.TaskSchemas import TaskInput, TaskOutput, TaskUpdate
+from src.Schemas.ToolSchemas import ToolOutput
 
 router = APIRouter(prefix="/api/v1/tasks", tags=["Tasks"])
 
@@ -40,3 +43,18 @@ async def update_task(task_id: int, task_data: TaskUpdate, token: str = Depends(
 async def delete_task(task_id: int, token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_session)):
     await AuthController.verify_token(token, session)
     return await TaskController.delete(task_id, session)
+
+
+@router.post("/{task_id}/add_comment/", response_model=CommentOutput)
+async def create_task(task_id: int, comment_data: CommentInput, session: AsyncSession = Depends(get_session)):
+    return await TaskController.create_comment(task_id, comment_data, session)
+
+
+@router.post("/{task_id}/add_point/", response_model=PointOutput)
+async def create_task(task_id: int, point_data: PointInput, session: AsyncSession = Depends(get_session)):
+    return await TaskController.create_point(task_id, point_data, session)
+
+
+@router.post("/{task_id}/append_tool/", response_model=ToolOutput)
+async def create_task(task_id: int, tool_id: int, session: AsyncSession = Depends(get_session)):
+    return await TaskController.add_tool(task_id, tool_id, session)
