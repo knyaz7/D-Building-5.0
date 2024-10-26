477b22eb0f5ee0cc980efe608fd33f3aed9fd4b4
knyaz7 <vn.guitar@mail.ru>
2024-10-26 13:37:51 +0300

    Various upd

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from src.Config.db import engine, Base
 from src.Routers.v1.user_router import router as user_router
 from src.Routers.v1.role_router import router as role_router
 from src.Routers.v1.auth_router import router as auth_router
-from src.Routers.v1.command_router import router as command_router
+from src.Routers.v1.master_task_router import router as master_task_router
 
 from src.Helpers.MiddlewareHelper import MiddlewareHelper
 
@@ -20,4 +20,4 @@ async def startup():
 app.include_router(role_router)
 app.include_router(user_router)
 app.include_router(auth_router)
-app.include_router(command_router)
+app.include_router(master_task_router)
