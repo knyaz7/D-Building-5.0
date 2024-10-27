91cde54ef360d786835acc7423dd419572b603d5
F-Shapiro <abstractxunxon@gmail.com>
2024-10-27 04:53:14 +0300

    update ai route

--- backend/src/Routers/v1/ai_router.py
+++ backend/src/Routers/v1/ai_router.py
@@ -11,9 +11,11 @@ from src.Config.db import get_session
 from src.Controllers.AuthController import AuthController, oauth2_scheme
 from src.Schemas.UserSchemas import UserOutput
 from src.Schemas.AiSchemas import AiInput
+
+from src.Models.UserMeta import UserMeta
 from src.Models.User import User
 from src.Models.MasterTask import MasterTask
-from src.Models.Tool import Tool
+from src.Models.Position import Position
 
 router = APIRouter(prefix="/api/v1/ai", tags=["ai"])
 
@@ -22,10 +24,26 @@ async def get_rec_users(master_task_id: int, ai_data: AiInput, token: str = Depe
     await AuthController.verify_token(token, session)
     query = await session.execute(select(User))
     users = query.scalars().all()
+    user_ids = [u.id for u in users]
+    indexed_users = {id:item for id, item in zip(user_ids, users)}
+
+    query = await session.execute(select(UserMeta).where(UserMeta.id.in_(user_ids)))
+    users_meta = query.scalars().all()
+
+    dict_users = []
+    for um in users_meta:
+        query = await session.execute(select(Position).where(Position.id == um.position_id))
+        position = query.scalar_one()
+
+        dict_users.append({
+            'fullname':indexed_users[um.user_id].fullname,
+            'position':position,
+            'description':um.description,
+            'stack':um.stack,
+            'rating':um.rating
+        })
+
     query = await session.execute(select(MasterTask).where(MasterTask.id == master_task_id))
     master_task = query.scalar_one()
-    query = await session.execute(select(Tool).where(Tool.id.in_(ai_data.stack)))
-    stack = query.scalars().all()
-    stack = [tool.name for tool in stack]
 
-    return await evaluate_employees(users, master_task.name, stack)
+    return await evaluate_employees(dict_users, master_task.name, ai_data.stack)
