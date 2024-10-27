f2381d1f20900bc2526b8df661426b11e495341c
F-Shapiro <abstractxunxon@gmail.com>
2024-10-27 06:49:40 +0300

    update ai route

--- backend/src/Routers/v1/ai_router.py
+++ backend/src/Routers/v1/ai_router.py
@@ -1,49 +1,16 @@
 from typing import List
-
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
-# когда teamlead создаёт master task
-from src.ml.rec_employees import evaluate_employees
 
 from src.Config.db import get_session
 from src.Controllers.AuthController import AuthController, oauth2_scheme
+from src.Controllers.AiController import AiController
 from src.Schemas.UserSchemas import UserOutput
 from src.Schemas.AiSchemas import AiInput
-
-from src.Models.UserMeta import UserMeta
-from src.Models.User import User
-from src.Models.MasterTask import MasterTask
-from src.Models.Position import Position
 
 router = APIRouter(prefix="/api/v1/ai", tags=["ai"])
 
 @router.post("/", response_model=List[UserOutput])  
 async def get_rec_users(ai_data: AiInput, token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_session)):
     await AuthController.verify_token(token, session)
-    query = await session.execute(select(User))
-    users = query.scalars().all()
-    user_ids = [u.id for u in users]
-    indexed_users = {id:item for id, item in zip(user_ids, users)}
-
-    query = await session.execute(select(UserMeta).where(UserMeta.id.in_(user_ids)))
-    users_meta = query.scalars().all()
-
-    dict_users = []
-    for um in users_meta:
-        query = await session.execute(select(Position).where(Position.id == um.position_id))
-        position = query.scalar_one()
-
-        dict_users.append({
-            'fullname':indexed_users[um.user_id].fullname,
-            'position':position.name,
-            'description':um.description,
-            'stack':um.stack,
-            'rating':um.rating
-        })
-
-    query = await session.execute(select(MasterTask).where(MasterTask.id == ai_data.master_task_id))
-    master_task = query.scalar_one()
-
-    return await evaluate_employees(dict_users, master_task.name, ai_data.stack)
+    return await AiController.get_rec_users(ai_data, session)
